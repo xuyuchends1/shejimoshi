46b062df85336e24200059251444163308f2d8c7
xuyuchends <xuyuchends@163.com>
2017-05-30 10:48:50 +0800

    d
    sd

---
+++ 原型模式/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 原型模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resume r1 = new Resume("aa");
+            r1.SetPersionInfo("1", 25);
+            r1.SetWorkExpersience("a", "b");
+
+            //深度clone
+            Resume r2 = (Resume)r1.Clone();
+
+            r1.name = "bb";
+            r1.SetPersionInfo("2", 20);
+            r1.SetWorkExpersience("c", "d");
+        }
+    }
+}
---
+++ 原型模式/Resume.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 原型模式
+{
+    // 简历
+    class Resume:ICloneable
+    {
+        public string name;
+        public string sex;
+        public int age;
+        public string timeArea;
+        public string company;
+
+        public Resume(string name)
+        {
+            this.name = name;
+        }
+        public void SetPersionInfo(string sex,int age)
+        {
+            this.sex = sex;
+            this.age = age;
+        }
+        public void SetWorkExpersience(string timeArea,string company)
+        {
+            this.timeArea = timeArea;
+            this.company = company;
+        }
+        public void Display()
+        {
+            Console.WriteLine("{0}{1}{2}", name, sex, age);
+            Console.WriteLine("{0}{1}", timeArea, company);
+        }
+
+        //深复制
+        public object Clone()
+        {
+            // 浅复制
+            return this.MemberwiseClone();
+        }
+    }
+}
