5bdcf1ba4d5653e375043448b290a6d8155b36aa
xuyuchends <xuyuchends@163.com>
2017-06-04 16:18:59 +0800

    dd
    ss

---
+++ 单例模式/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 单例模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.getInstance();
+            Singleton s2 = Singleton.getInstance();
+
+        }
+    }
+}
---
+++ 单例模式/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 单例模式
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        private Singleton()
+        { }
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
